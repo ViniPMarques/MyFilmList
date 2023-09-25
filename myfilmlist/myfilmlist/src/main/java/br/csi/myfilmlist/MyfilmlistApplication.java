7692e7109279132482a02129ba30da1c8bc5a4df
Usagii <129550928+ViniPMarques@users.noreply.github.com>
2023-09-25 16:40:54 -0300

    start

---
+++ myfilmlist/myfilmlist/src/main/java/br/csi/myfilmlist/MyfilmlistApplication.java
@@ -0,0 +1,13 @@
+package br.csi.myfilmlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfilmlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfilmlistApplication.class, args);
+	}
+
+}
---
+++ myfilmlist/myfilmlist/src/test/java/br/csi/myfilmlist/MyfilmlistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.csi.myfilmlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfilmlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
